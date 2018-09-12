8c811e53563f84a001605400ef64bbb63317606b
Marco <mgenova03@gmail.com>
2018-09-12 15:06:53 -0400

    create app and install material-ui

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>OSRS</h2>
+          <Button variant="contained" color="primary">
+              Hello World
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default App;
